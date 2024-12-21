0d80c0d22ebe2c70047c572825f9d0ce1f0fca83
CodeBoss420 <smfojle@gmail.com>
2024-12-21 08:56:32 +0000

    day-1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,37 +1,24 @@
+#day-1
 import sys
 import pygame
-from setting import Settings
 
-
-class AlienInvasion:
-    """Overall class to manage game assets and behavior"""
+class AlienInavsion:
+    """Overall class to manage game assets and behavior. """
     def __init__(self):
         pygame.init()
-        self.clock = pygame.time.Clock()
-        self.settings = Settings()
-
-
-
-
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-        
-
     def run_game(self):
+        """start the main loop for the game"""
         while True:
+            #Watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.settings.bg_color)                    
+
+            #make it most recently drawn screen visible
             pygame.display.flip()
-            self.clock.tick(60)
 if __name__ == "__main__":
-    ai = AlienInvasion()
-    ai.run_game()                                           
-
-
-
-
-
-
-
+    #make a game insatance , and run the game
+    ai = AlienInavsion()
+    ai.run_game()                                    
